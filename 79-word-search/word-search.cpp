3ee52b81b739f0914fbc85ed9493173204195a92
ns01234 <146398022+ns01234@users.noreply.github.com>
2024-04-03 07:37:29 +0300

    Time: 1381 ms (6.73%) | Memory: 383.8 MB (5.00%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,40 +1,35 @@
 #define ll long long
-
 class Solution {
 public:
     int arr[4][2] = {{1,0},{0,1},{0,-1},{-1,0}};
-
-    bool dfs(vector<vector<char>>& board, string& word, int i, int j, int pos) {
-        if (pos == word.size()) return true; // If the entire word is found
-        
-        if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || board[i][j] != word[pos]) {
-            return false; // Out of bounds or mismatch
-        }
-
-        char temp = board[i][j]; // Temporarily store the current character
-        board[i][j] = '*'; // Mark the current cell as visited
-        
-        // Explore in all four directions
-        for (auto x : arr) {
-            int ni = i + x[0];
-            int nj = j + x[1];
-            if (dfs(board, word, ni, nj, pos + 1)) return true; // If found in any direction, return true
-        }
-
-        board[i][j] = temp; // Restore the character
-        return false; // If word is not found
-    }
-
-    bool exist(vector<vector<char>>& board, string& word) {
+    bool exist(vector<vector<char>>& board, string word) {
         int r = board.size();
         int c = board[0].size();
-        
-        // Iterate through the board
-        for (int i = 0; i < r; ++i) {
-            for (int j = 0; j < c; ++j) {
-                if (dfs(board, word, i, j, 0)) return true; // Start DFS from each cell
+        for (int i = r-1; ~i; --i) {
+            for (int j = c-1; ~j; --j) {
+                if (board[i][j] != word[0]) continue;
+                queue<array<ll,3>> q;
+                q.push({i*c+j,0,0});
+                while (q.size()) {
+                    auto p = q.front();
+                    q.pop();
+                    int n = p[0];
+                    int st = p[1];
+                    ll msk = p[2];
+                    int rr = n / c;
+                    int cc = n % c;
+                    if ((1LL << n) & msk) continue;
+                    msk |= (1LL << n);
+                    if (st >= word.size() || word[st] != board[rr][cc]) continue;
+                    if (word[st] == board[rr][cc] && st == word.size() - 1) return 1;
+                    for (auto x : arr) {
+                        int nr = x[0] + rr;
+                        int nc = x[1] + cc;
+                        if (~nr && nr < r && ~nc && nc < c) q.push({nr*c+nc,st+1,msk});
+                    }
+                }
             }
         }
-        return false; // If word is not found anywhere
+        return 0;
     }
 };
