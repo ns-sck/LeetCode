eff197f4415bff0d18ff48d3c8cea6a1b4e800dc
ns01234 <146398022+ns01234@users.noreply.github.com>
2024-04-03 09:41:52 +0300

    Time: 1575 ms (5.55%) | Memory: 383.2 MB (5.00%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -8,11 +8,11 @@ public:
         for (int i = r-1; ~i; --i) {
             for (int j = c-1; ~j; --j) {
                 if (board[i][j] != word[0]) continue;
-                queue<array<ll,3>> q;
-                q.push({i*c+j,0,0});
-                while (q.size()) {
-                    auto [n, st, msk] = q.front();
-                    q.pop();
+                stack<array<ll,3>> s;
+                s.push({i*c+j,0,0});
+                while (s.size()) {
+                    auto [n, st, msk] = s.top();
+                    s.pop();
                     int rr = n / c;
                     int cc = n % c;
                     if ((1LL << n) & msk) continue;
@@ -22,7 +22,7 @@ public:
                     for (auto& x : arr) {
                         int nr = x[0] + rr;
                         int nc = x[1] + cc;
-                        if (~nr && nr < r && ~nc && nc < c) q.push({nr*c+nc,st+1,msk});
+                        if (~nr && nr < r && ~nc && nc < c) s.push({nr*c+nc,st+1,msk});
                     }
                 }
             }
