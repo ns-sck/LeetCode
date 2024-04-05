a2ebba8a585434b8e953085521dfd6d1b3ddb733
ns01234 <146398022+ns01234@users.noreply.github.com>
2024-04-06 02:15:38 +0300

    Time: 1583 ms (7.73%) | Memory: 29.7 MB (25.30%) - LeetSync

---
+++ 127-word-ladder/word-ladder.cpp
@@ -0,0 +1,45 @@
+#define w wordList
+const int _max = 5e3;
+class Solution {
+public:
+    bool dff(string& s1, string& s2) {
+        bool b = 0;
+        if (s1 == s2) return 0;
+        for (int i = 0; i < s1.size(); ++i) {
+            if (s1[i] != s2[i]) {
+                if (b) return false;
+                b = 1;
+            } 
+        }
+        return 1;
+    }
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        vector<int> dp(_max+1, 1e9);
+        vector<vector<int>> adj(_max+1, vector<int>());
+        int sz = wordList.size();
+        for (int i = 0; i < sz; ++i) {
+            if (dff(beginWord, w[i])) adj[_max].push_back(i);
+            for (int j = 0; j < sz; ++j) {
+                if (dff(w[i], w[j])) {
+                    adj[i].push_back(j);
+                }
+            }
+        }
+        int ans = 1e9;
+        queue<array<int, 2>> q;
+        q.push({_max, 1});
+        while (q.size()) {
+            int idx = q.front()[0];
+            int st = q.front()[1];
+            q.pop();
+            if (dp[idx] < st) continue;
+            if (idx != _max && w[idx] == endWord) ans = min(ans, st);
+            dp[idx] = st;
+            for (auto x : adj[idx]) {
+                if (dp[x] > st+1) q.push({x, st+1});
+            }
+        }
+        if (ans == 1e9) return 0;
+        return ans;
+    }
+};
