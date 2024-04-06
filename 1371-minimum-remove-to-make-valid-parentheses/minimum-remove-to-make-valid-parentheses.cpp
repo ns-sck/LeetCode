d9a2840af95cb582fa3440c625aa7af861eed533
ns01234 <146398022+ns01234@users.noreply.github.com>
2024-04-06 06:14:58 +0300

    Time: 32 ms (10.36%) | Memory: 14 MB (20.75%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -1,35 +1,24 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        stack<char> st;
-        for (int i = 0; i < s.length(); i++) {
+        vector<bool> b(1e5);
+        string ans = "";
+        stack<int> st;
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == '(') st.push(i);
             if (s[i] == ')') {
-                if (!st.empty() && st.top() == '(') {
+                if (st.size()) {
+                    b[i] = b[st.top()] = 1;
                     st.pop();
-                } else if (st.empty()){
-                    s[i] = '.';
                 }
-            } else if (s[i] == '(') {
-                st.push(s[i]);
             }
         }
-
-        for (int i = s.size()-1; i >= 0; i--) {
-            if (!st.empty() && s[i] == '(') {
-                s[i] = '.';
-                st.pop();
-            } else if (st.empty()) {
-                break;
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == ')' || s[i] == '(') {
+                if (b[i]) ans += s[i];
             }
+            else ans += s[i];
         }
-
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == '.') {
-                s.erase(s.begin() + i);
-                i--;
-            }
-        }
-
-        return s;
+        return ans;
     }
 };
