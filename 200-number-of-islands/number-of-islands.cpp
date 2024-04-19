db06ac80fb4d7aba7f53daa4ee1a96e8229e27ca
ns01234 <146398022+ns01234@users.noreply.github.com>
2024-04-20 00:14:19 +0300

    Time: 32 ms (39.45%) | Memory: 17 MB (41.02%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -6,7 +6,7 @@ public:
         ios::sync_with_stdio;
         int r = grid.size();
         int c = grid[0].size();
-        stack<pair<int,int>> q;
+        queue<pair<int,int>> q;
         vector<vector<bool>> vis(r, vector<bool>(c));
         int ans = 0;
         for (int i = 0; i < r; ++i)
@@ -16,8 +16,8 @@ public:
                     vis[i][j] = 1;
                     q.push({i,j});
                     while (q.size()) {
-                        int rr = q.top().first;
-                        int cc = q.top().second;
+                        int rr = q.front().first;
+                        int cc = q.front().second;
                         q.pop();
                         for (int i = 0; i < 4; ++i) {
                             int nr = rr + rws[i];
