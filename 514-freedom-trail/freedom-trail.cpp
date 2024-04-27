cf23e91317055999ed5dbcca8d89eaf555f844ed
ns01234 <146398022+ns01234@users.noreply.github.com>
2024-04-27 12:22:36 +0300

    Time: 401 ms (5.19%) | Memory: 98.6 MB (9.20%) - LeetSync

---
+++ 514-freedom-trail/freedom-trail.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int findRotateSteps(string ring, string key) {
+        vector<vector<int>> t(26);
+        int sz = ring.size();
+        for (int i = 0; i < ring.size(); ++i) {
+            int c = ring[i] - 'a';
+            t[c].push_back(i);
+        }
+        vector<vector<int>> dp(ring.size(), vector<int>(key.size(), -1));
+        vector<int> dp2(sz, -1);
+        queue<array<int, 3>> q;
+        for (int i = 0; i < sz; ++i) {
+            if (ring[i] == key[0]) {
+                q.push({i, 0, min(i, sz-i)});
+            }
+        }
+        int ans = 1e9;
+        while (q.size()) {
+            auto [idx_r, idx_k, c] = q.front();
+            q.pop();
+            if (idx_k == key.size()-1) {
+                ans = min(ans, c);
+                continue;
+            }
+            int next = key[idx_k+1] - 'a';
+            for (auto i : t[next]) {
+                int dff = min(abs(idx_r-i), sz-(abs(idx_r-i)));
+                if (dp[i][idx_k+1] == -1 || dp[i][idx_k+1] > c+dff || dp2[i] < idx_k+1) {
+                    q.push({i, idx_k+1, c+dff});
+                    dp[i][idx_k+1] = c+dff;
+                    dp2[i] = idx_k+1;
+                }
+            }
+        }
+        return ans + key.size();
+    }
+};
