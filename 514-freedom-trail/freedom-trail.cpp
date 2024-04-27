376a9bc84f5d644403850c93d1af11ece3a0d244
ns01234 <146398022+ns01234@users.noreply.github.com>
2024-04-27 12:30:22 +0300

    Time: 398 ms (5.19%) | Memory: 98.6 MB (9.20%) - LeetSync

--- 514-freedom-trail/freedom-trail.cpp
+++ 514-freedom-trail/freedom-trail.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int findRotateSteps(string ring, string key) {
+        ios::sync_with_stdio;
         vector<vector<int>> t(26);
         int sz = ring.size();
         for (int i = 0; i < ring.size(); ++i) {
             int c = ring[i] - 'a';
             t[c].push_back(i);
         }
-        vector<vector<int>> dp(ring.size(), vector<int>(key.size(), -1));
+        vector<vector<int>> dp(sz, vector<int>(key.size(), -1));
         vector<int> dp2(sz, -1);
         queue<array<int, 3>> q;
         for (int i = 0; i < sz; ++i) {
@@ -16,6 +17,7 @@ public:
             }
         }
         int ans = 1e9;
+        // array<int, 2> dp3 = {0, 1e9};
         while (q.size()) {
             auto [idx_r, idx_k, c] = q.front();
             q.pop();
@@ -26,7 +28,7 @@ public:
             int next = key[idx_k+1] - 'a';
             for (auto i : t[next]) {
                 int dff = min(abs(idx_r-i), sz-(abs(idx_r-i)));
-                if (dp[i][idx_k+1] == -1 || dp[i][idx_k+1] > c+dff || dp2[i] < idx_k+1) {
+                if ((dp[i][idx_k+1] == -1 || dp[i][idx_k+1] > c+dff) && dp2[i] <= idx_k+1) {
                     q.push({i, idx_k+1, c+dff});
                     dp[i][idx_k+1] = c+dff;
                     dp2[i] = idx_k+1;
