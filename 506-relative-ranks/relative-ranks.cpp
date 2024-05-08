41957bdbb0effe458814b0696ed1d017861b77c3
ns01234 <146398022+ns01234@users.noreply.github.com>
2024-05-08 13:22:29 +0300

    Time: 8 ms (66.81%) | Memory: 16.8 MB (15.55%) - LeetSync

---
+++ 506-relative-ranks/relative-ranks.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& s) {
+        int sz = s.size();
+        vector<array<int, 2>> v;
+        for (int i = 0; i < sz; ++i) {
+            v.push_back({s[i], i});
+        }
+        sort(v.rbegin(), v.rend());
+        vector<string> ans(sz);
+        for (int i = 0; i < sz; ++i) {
+            int idx = v[i][1];
+            if (i == 0) ans[idx] = "Gold Medal";
+            else if (i == 1) ans[idx] = "Silver Medal";
+            else if (i == 2) ans[idx] = "Bronze Medal";
+            else ans[idx] = to_string(i+1);
+        }
+        return ans;
+    }
+};
