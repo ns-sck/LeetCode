1edab600aa896905de10df3bd8364f26497fe235
ns01234 <146398022+ns01234@users.noreply.github.com>
2024-05-14 18:50:31 +0300

    Time: 120 ms (67.89%) | Memory: 9.1 MB (63.16%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.cpp
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.cpp
@@ -3,7 +3,7 @@ public:
     int rr[4] = {1,0,-1,0};
     int cc[4] = {0,1,0,-1};
     int r1, c1;
-    int dfs (vector<vector<bool>>& act, int i, int j, int t, vector<vector<int>>& grid) {
+    int dfs (int** act, int i, int j, int t, vector<vector<int>>& grid) {
         int x = t;
         act[i][j] = 1;
         for (int k = 0; k < 4; ++k) {
@@ -22,7 +22,12 @@ public:
         int c = grid[0].size();
         r1 = r, c1 = c;
         int ans = 0;
-        vector<vector<bool>> act(r, vector<bool>(c));
+        int** act = new int*[r];
+        for (int i = 0; i < r; ++i) {
+            act[i] = new int[c];
+            memset(act[i], 0, sizeof(int) * c); // Initialize each row of the array with zeros
+        }
+        // vector<vector<bool>> act(r, vector<bool>(c));
         for (int i = 0; i < r; ++i)
             for (int j = 0; j < c; ++j) {
                 if (!grid[i][j]) continue;
