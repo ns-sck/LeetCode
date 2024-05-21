c764c2161e2a8d39e908060eadb09dc676ca61fe
ns01234 <146398022+ns01234@users.noreply.github.com>
2024-05-21 22:13:35 +0300

    Time: 4 ms (39.89%) | Memory: 8.4 MB (62.90%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        set<int> s;
+        vector<vector<int>> ans;
+        for (auto x : nums) s.insert(x);
+        for (int i = 0; i < (1 << s.size()); ++i) {
+            vector<int> v;
+            for (int j = 0; j < s.size(); ++j) {
+                if (i & (1 << j)) v.push_back(nums[j]);
+            }
+            ans.push_back(v);
+        }
+        return ans;
+    }
+};
