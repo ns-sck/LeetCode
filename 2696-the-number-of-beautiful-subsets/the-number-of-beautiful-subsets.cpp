946c92a8e525030034ceb6838dfea29ab64bc76b
ns01234 <146398022+ns01234@users.noreply.github.com>
2024-05-24 01:11:15 +0300

    Time: 424 ms (41.55%) | Memory: 35.7 MB (96.33%) - LeetSync

---
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int ans = -1;
+    int beautifulSubsets(vector<int>& nums, int k) {
+        dfs(nums, 0, 0, k);
+        return ans; 
+    }
+    void dfs(vector<int>& v, int l, int mask, int k) {
+        if (l == v.size()) {
+            ++ans; return;
+        }
+        dfs(v, l+1, mask, k);
+        bool b = 0;
+        for (int i = 0; i < v.size(); ++i) if (((1 << i) & mask) && abs(v[i]-v[l]) == k) b = 1;
+        if (!b) dfs(v, l+1, mask | (1 << l), k);
+    }
+};
