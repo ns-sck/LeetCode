52dd0c126731abb05dd990cb489aba9f2a885a28
Enes <146398022+ns01234@users.noreply.github.com>
2024-05-27 22:33:40 +0300

    Time: 50 ms (83.82%) | Memory: 12.5 MB (84.75%) - LeetSync

--- 552-student-attendance-record-ii/student-attendance-record-ii.cpp
+++ 552-student-attendance-record-ii/student-attendance-record-ii.cpp
@@ -6,8 +6,12 @@ public:
     ll dp[100001][2][3];
 
     int checkRecord(int n) {
+
+        ios::sync_with_stdio(0), cin.tie(0);
+
         fill(&dp[0][0][0], &dp[0][0][0] + sizeof(dp) / sizeof(ll), 0);
         dp[0][0][0] = dp[0][1][0] = dp[0][0][1] = 1;
+
         for (int i = 1; i < n; ++i) {
             dp[i][0][0] = (dp[i][0][0] + dp[i-1][0][0] + dp[i-1][0][1] + dp[i-1][0][2]) % MOD;
             dp[i][0][1] = (dp[i][0][1] + dp[i-1][0][0]) % MOD;
@@ -17,6 +21,7 @@ public:
             dp[i][1][1] = (dp[i][1][1] + dp[i-1][1][0]) % MOD;
             dp[i][1][2] = (dp[i][1][2] + dp[i-1][1][1]) % MOD;
         }
+        
         return (dp[n-1][0][0] + dp[n-1][0][1] + dp[n-1][0][2] + dp[n-1][1][0] + dp[n-1][1][1] + dp[n-1][1][2]) % MOD;
     }
 };
