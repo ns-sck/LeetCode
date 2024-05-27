4c994d1198f706d84fa04d420fd98ec163070e3d
Enes <146398022+ns01234@users.noreply.github.com>
2024-05-27 22:33:01 +0300

    Time: 42 ms (87.21%) | Memory: 12.3 MB (86.59%) - LeetSync

--- 552-student-attendance-record-ii/student-attendance-record-ii.cpp
+++ 552-student-attendance-record-ii/student-attendance-record-ii.cpp
@@ -1,21 +1,22 @@
+#define ll long long
 class Solution {
 public:
-    // not my solution
-    const long long mod=1e9+7;
-    long long dp[100001][2][3];
-    long long f(int i, int absent, int late){
-    //    if (absent>=2 || late>=3) return 0;
-        if (i==0) return 1;
-        if (dp[i][absent][late]!=-1) return dp[i][absent][late];
 
-        long long ans=f(i-1, absent, 0);// P
-        ans+=(late<2?f(i-1, absent, late+1):0);// L
-        ans+=(absent==0?f(i-1, absent+1, 0):0);// A
-       
-        return dp[i][absent][late]=ans%mod;
-    }
+    const ll MOD = 1e9+7;
+    ll dp[100001][2][3];
+
     int checkRecord(int n) {
-        fill(&dp[0][0][0], &dp[0][0][0]+(n+1)*6, -1);
-        return f(n, 0, 0);
+        fill(&dp[0][0][0], &dp[0][0][0] + sizeof(dp) / sizeof(ll), 0);
+        dp[0][0][0] = dp[0][1][0] = dp[0][0][1] = 1;
+        for (int i = 1; i < n; ++i) {
+            dp[i][0][0] = (dp[i][0][0] + dp[i-1][0][0] + dp[i-1][0][1] + dp[i-1][0][2]) % MOD;
+            dp[i][0][1] = (dp[i][0][1] + dp[i-1][0][0]) % MOD;
+            dp[i][0][2] = (dp[i][0][2] + dp[i-1][0][1]) % MOD;
+            dp[i][1][0] = (dp[i][1][0] + dp[i-1][0][0] + dp[i-1][0][1] + dp[i-1][0][2]
+                         + dp[i-1][1][0] + dp[i-1][1][1] + dp[i-1][1][2]) % MOD;
+            dp[i][1][1] = (dp[i][1][1] + dp[i-1][1][0]) % MOD;
+            dp[i][1][2] = (dp[i][1][2] + dp[i-1][1][1]) % MOD;
+        }
+        return (dp[n-1][0][0] + dp[n-1][0][1] + dp[n-1][0][2] + dp[n-1][1][0] + dp[n-1][1][1] + dp[n-1][1][2]) % MOD;
     }
 };
