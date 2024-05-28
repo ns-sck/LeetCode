702d5ce659731585671a1db05f6a1a71c6145048
Enes <146398022+ns01234@users.noreply.github.com>
2024-05-28 11:42:34 +0300

    Time: 12 ms (6.30%) | Memory: 9.2 MB (17.13%) - LeetSync

---
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int equalSubstring(string s, string t, int c) {
+        int sz = s.size();
+        int prfx[sz+1];
+
+        prfx[0] = 0; prfx[1] = abs(s[0] - t[0]);
+        for (int i = 2; i <= sz; ++i)
+            prfx[i] = abs(s[i-1] - t[i-1]) + prfx[i-1];
+
+        int ans = 0;
+        for (int i = 1; i <= sz; ++i) {
+            int l = i, h = sz;
+            while (l <= h) {
+                int m = (l+h) / 2;
+                if (prfx[m] - prfx[i-1] <= c) {
+                    ans = max(ans, m-i+1);
+                    l = m+1;
+                } else h = m-1;
+            }
+        }
+        return ans;
+    }
+};
