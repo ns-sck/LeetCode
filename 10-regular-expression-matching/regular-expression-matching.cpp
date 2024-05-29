5d1a77854b5178e681583b3b2b495eb956680af6
Enes <146398022+ns01234@users.noreply.github.com>
2024-05-29 23:42:25 +0300

    Time: 3 ms (69.21%) | Memory: 8.8 MB (44.53%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int n = s.size(), m = p.size();
+        vector<vector<bool>> dp(n+1, vector<bool>(m+1));
+        dp[0][0] = 1;
+        
+        for (int i = 0; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                bool b = 0;
+                if (p[j-1] == '*') {
+                    b |= j>1 && dp[i][j-2];
+                    b |= i && (dp[i-1][j] && (s[i-1] == p[j-2] || p[j-2] == '.'));
+                } else {
+                    b |= i && ((dp[i-1][j-1]) && (s[i-1] == p[j-1] || p[j-1] == '.'));
+                }
+                dp[i][j] = b; 
+            }
+        }
+        return dp[n][m];
+    }
+};
