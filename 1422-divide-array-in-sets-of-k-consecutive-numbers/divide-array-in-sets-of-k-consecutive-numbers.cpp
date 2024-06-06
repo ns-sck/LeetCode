37c225d5c3489d848eea60e1472fd8313aaa2973
Enes <146398022+ns-sck@users.noreply.github.com>
2024-06-07 02:23:10 +0300

    Time: 86 ms (96.37%) | Memory: 53.5 MB (40.86%) - LeetSync

---
+++ 1422-divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPossibleDivide(vector<int>& v, int g) {
+        ios::sync_with_stdio(0);
+        if (v.size()%g) return 0;
+        
+        map<int, int> mp;
+        for (auto& x : v) ++mp[x];
+
+        while (mp.size()) {
+            auto it = mp.begin();
+            for (int i = 0; i < g; ++i) {
+                if (it == mp.end()) return 0;
+                int x = it->first;
+                --it->second;
+                if (it->second <= 0) {
+                    auto it2 = it;
+                    ++it;
+                    mp.erase(it2);
+                } else ++it;
+                if (i^g-1 && it != mp.end() && it->first ^ (x+1)) return 0;
+            }
+        }
+        return 1;
+    }
+};  
