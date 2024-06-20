27cf3961d6ff245aff09a9ffdeef1f7ac3aa4f86
Enes <146398022+ns-sck@users.noreply.github.com>
2024-06-21 02:38:08 +0300

    Time: 77 ms (99.69%) | Memory: 61.5 MB (36.17%) - LeetSync

---
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxDistance(vector<int>& p, int m) {
+        ios::sync_with_stdio(0);
+
+        sort(p.begin(), p.end());
+        int N = p.size();
+        
+        int lo = 0, hi = p[N-1]-p[0]+1;
+        while (lo < hi) {
+            int md = (lo+hi)>>1;
+            int c = 1, l = p[0];
+            for (int i = 1; i < N; ++i) {
+                if (p[i]-l >= md) {
+                    ++c;
+                    l = p[i];
+                }
+            }
+            if (c >= m) lo = md+1;
+            else hi = md;
+        }
+        return --lo;
+    }
+};
