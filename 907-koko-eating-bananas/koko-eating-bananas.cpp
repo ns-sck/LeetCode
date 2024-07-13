3ba0057d0bfe48b52cdaee029acf4c399835a903
Enes <146398022+ns-sck@users.noreply.github.com>
2024-07-13 14:07:15 +0300

    Time: 22 ms (95.46%) | Memory: 22.6 MB (8.15%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& p, int h) {
+        ios::sync_with_stdio(0);
+        sort(p.begin(), p.end());
+        int N = p.size();
+        int lo = 1, hi = p[N-1];
+        while (lo < hi) {
+            int mi = (lo+hi)>>1;
+            int c = 0;
+            for (int i = 0; i < N; ++i) {
+                int x = p[i]/mi;
+                c += x + (p[i]%mi != 0) ;
+            }
+            cout << c;
+            if (c > h) {
+                lo = mi+1;
+            } else {
+                hi = mi;
+            }
+        }
+        return lo;
+    }
+};
